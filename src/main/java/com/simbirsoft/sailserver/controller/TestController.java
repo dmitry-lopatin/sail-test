15eca8810d286756fc74e13ca58591ffccd98ec5
dmitry.lopatin <dmitry.lopatin@simbirsoft.com>
2017-04-08 18:45:20 +0300

    basic-api-implemented

--- src/main/java/com/simbirsoft/sailserver/controller/TestController.java
+++ src/main/java/com/simbirsoft/sailserver/controller/TestController.java
@@ -1,47 +1,78 @@
 package com.simbirsoft.sailserver.controller;
 
-import javafx.geometry.Pos;
+import com.simbirsoft.sailserver.entity.Order;
+import com.simbirsoft.sailserver.entity.Product;
+import com.simbirsoft.sailserver.entity.ProductCategory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.simbirsoft.sailserver.dao.PostgresDAO;
-import com.simbirsoft.sailserver.entity.DAOResponce;
 
 @Controller
 public class TestController {
 
-    @RequestMapping(value = "/getAll" , method = RequestMethod.GET, produces = "application/json")
+    @Autowired
+    PostgresDAO postgresDAO;
+
+    @RequestMapping(value = "/getAllCategories" , method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public List<DAOResponce> getAllData() {
-        PostgresDAO postgresDAO= new PostgresDAO();
-        List<DAOResponce> responces = postgresDAO.getAllData();
-        return responces;
+    public List<String> getAllCategories() {
+        List<String> response = postgresDAO.getAllCategories();
+        return response;
     }
-    @RequestMapping(value = "/getAllDAta" , method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/getAllProducts", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public DAOResponce getAllAddresses() {
-        DAOResponce responce = new DAOResponce();
-        responce.setName("Addresses");
-        return responce;
+    public List<Product> getAllProducts() {
+        List<Product> response = postgresDAO.getAllProducts();
+        return response;
     }
-    @RequestMapping(value = "/getCategories" , method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/getAllOrders", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public DAOResponce getCategoriesByAdress(String address) {
-        DAOResponce responce = new DAOResponce();
-        responce.setName("Categories");
-        responce.setValue(address);
-        return responce;
+    public List<Order> getAllOrders() {
+        List<Order> response = postgresDAO.getAllOrders();
+        return response;
     }
-    @RequestMapping(value = "/getCost" , method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/getAllAddresses", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public DAOResponce getCost() {
-        DAOResponce responce = new DAOResponce();
-        responce.setName("Cost");
-        return responce;
+    public List<String> getAllAddresses() {
+        List<String> response = postgresDAO.getAllAddresses();
+        return response;
+    }
+    @RequestMapping(value = "/getCategoriesByAddress", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<String> getCategoriesByAddress(String address) {
+        List<String> response = postgresDAO.getCategoriesByAddress(address);
+        return response;
+    }
+
+    @RequestMapping(value = "/getCategoriesByAddressList", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<String> getCategoriesByAddressList(String[] addressList) {
+        List<String> response = postgresDAO.getCategoriesByAddressList(Arrays.asList(addressList));
+        return response;
+    }
+
+    @RequestMapping(value = "/getAddressesByCategoryList", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<String> getAddressesByCategoryList(String[] categoryList) {
+        List<String> response = postgresDAO.getAddressesByCategoryList(Arrays.asList(categoryList));
+        return response;
+    }
+
+    @RequestMapping(value = "/getSumByAddressListAndCategoryList", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Double getSumByAddressListAndCategoryList(String[] addresses, String[] categories) {
+        List<String> addressList = addresses == null ? getAllAddresses() : Arrays.asList(addresses);
+        List<String> categoryList = categories == null ? getAllCategories() : Arrays.asList(categories);
+        Double response = postgresDAO.getSumByAddressListAndCategoryList(addressList, categoryList);
+        return response;
     }
 }
---
+++ src/main/java/com/simbirsoft/sailserver/entity/Order.java
@@ -0,0 +1,36 @@
+package com.simbirsoft.sailserver.entity;
+
+import java.util.Date;
+
+public class Order {
+
+    private int orderId;
+
+    private Date orderDate;
+
+    private String orderAddress;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderAddress() {
+        return orderAddress;
+    }
+
+    public void setOrderAddress(String orderAddress) {
+        this.orderAddress = orderAddress;
+    }
+}
